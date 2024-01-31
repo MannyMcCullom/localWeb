9e9b7c85b696e91cd0bb429764fa9c21d28dbb55
MannyMcCullom <m.mccullom11@gmail.com>
2024-01-31 12:12:03 -0500

    Initial Page

---
+++ app.js
@@ -0,0 +1,74 @@
+// Variables --------------------------------------------
+const haircuts = [
+    {name: 'fade', picture:"haircut1.jpg"},
+    {name: 'fade', picture:"haircut2.jpg"},
+];
+
+function loadCuts(arr, num = 0) {
+    let count = num;
+    while (num >= 1) {
+        arr.push({name: 'fade', picture:"haircut1.jpg"})
+        num--;
+    }
+}
+
+loadCuts(haircuts, 7);
+
+const title = document.querySelector(".headerContainer");
+title.addEventListener("click", ()=>{
+    console.log("clicked:", title.innerHTML);
+    location.reload();
+})
+title.addEventListener("mouseenter", ()=>{
+    console.log("entered:")
+    // title.classList.toggle("pointer");
+    // title.classList.toggle("whiteOut");
+    title.classList.toggle("mouseOn");
+
+})
+
+title.addEventListener("mouseleave", ()=>{
+    console.log("left:")
+    // title.classList.toggle("pointer");
+    // title.classList.toggle("whiteOut");
+    title.classList.toggle("mouseOn");
+
+})
+
+const menu = document.querySelector("#menuContainer");
+
+for (item of haircuts) {
+    const menuItem = document.createElement("div");
+    menuItem.classList.add("menuItem")
+    const pic = document.createElement("img");
+    pic.setAttribute("src", item.picture);
+    pic.classList.add("picture");
+    menuItem.append(pic);
+    const text = document.createElement("div");
+    text.classList.add("text");
+    text.textContent = item.name;
+    menuItem.append(text);
+
+    menuItem.addEventListener("mouseenter", ()=>{
+        console.log("entered:")
+        // menuItem.classList.toggle("pointer");
+        menuItem.classList.toggle("whiteOut");
+        menuItem.classList.toggle("mouseOn");
+    })
+
+    menuItem.addEventListener("mouseleave", ()=>{
+        console.log("left:")
+        // menuItem.classList.toggle("pointer");
+        menuItem.classList.toggle("whiteOut");
+        menuItem.classList.toggle("mouseOn");
+        text.innerHTML = item.name;
+    })
+
+    menuItem.addEventListener("click", ()=>{
+        console.log("cliked:")
+    })
+
+    menu.appendChild(menuItem);
+}
+
+const header = document.querySelector('#headerContainer');
