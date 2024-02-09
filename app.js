ed568aabdcdda24c0a41a26b26aa58e1833cf8d2
MannyMcCullom <m.mccullom11@gmail.com>
2024-02-09 17:35:42 -0500

    some restructuring

--- app.js
+++ app.js
@@ -3,44 +3,24 @@ const haircuts = [
     {name: 'fade', picture:"haircut1.jpg"},
     {name: 'fade', picture:"haircut2.jpg"},
 ];
+const menu = document.querySelector("#menuItems");
+const mainImage = document.querySelector("#displayedImage");
 
-function loadCuts(arr, num = 0) {
+function loadCuts(arr, num = (arr.length - 1)) {
+    console.log(num);
     let count = num;
-    while (num >= 1) {
-        arr.push({name: 'fade', picture:"haircut1.jpg"})
+    while (num >= 0) {
+        arr.push({name: "fade", picture:"haircut1.jpg"})
         num--;
     }
 }
 
-loadCuts(haircuts, 17);
-
-const title = document.querySelector(".headerContainer");
-title.addEventListener("click", ()=>{
-    console.log("clicked:", title.innerHTML);
-    location.reload();
-})
-title.addEventListener("mouseenter", ()=>{
-    console.log("entered:")
-    // title.classList.toggle("pointer");
-    // title.classList.toggle("whiteOut");
-    title.classList.toggle("mouseOn");
-
-})
-
-title.addEventListener("mouseleave", ()=>{
-    console.log("left:")
-    // title.classList.toggle("pointer");
-    // title.classList.toggle("whiteOut");
-    title.classList.toggle("mouseOn");
-
-})
-
-const menu = document.querySelector("#menuItems");
+loadCuts(haircuts, 5);
 
 for (item of haircuts) {
     const menuItem = document.createElement("div");
     const pic = item.picture;
-    menuItem.classList.add("menuItem")
+    menuItem.classList.add("menuItem");
     const picEl = document.createElement("img");
     picEl.setAttribute("src", pic);
     picEl.classList.add("picture");
@@ -49,39 +29,26 @@ for (item of haircuts) {
     text.classList.add("text");
     text.textContent = item.name;
     menuItem.append(text);
-
     menuItem.addEventListener("mouseenter", ()=>{
-        console.log("entered:")
-        // menuItem.classList.toggle("pointer");
-        // menuItem.classList.toggle("whiteOut");
         menuItem.classList.toggle("mouseOn");
     })
 
     menuItem.addEventListener("mouseleave", ()=>{
-        console.log("left:")
-        // menuItem.classList.toggle("pointer");
-        // menuItem.classList.toggle("whiteOut");
         menuItem.classList.toggle("mouseOn");
         text.innerHTML = item.name;
     })
 
     menuItem.addEventListener("click", ()=>{
         changeImage(pic);
-        console.log("cliked:")
         menuItem.classList.add("whiteOut");
         setTimeout(()=>{
             menuItem.classList.remove("whiteOut");
         },250)
-        console.log(pic);
     })
 
     menu.appendChild(menuItem);
 }
 
-const header = document.querySelector('#headerContainer');
-
-const mainImage = document.querySelector("#displayedImage");
 function changeImage(newImage) {
     mainImage.setAttribute("src", newImage);
-} 
-changeImage("haircut2.jpg");
+}
